d2f35000b9a605e58cece7bfd5fe16a0b110d48a
SawyerSW <793439508@qq.com>
2017-03-29 15:18:32 +0800

    Update check_time.sh

--- check_time.sh
+++ check_time.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #截取ip，以:或一个多个空格作为分隔符，匹配到inet addr关键行，作条件判断，如果ip不是以127、192、10、172开头就截取出来
 ip=`/sbin/ifconfig | awk -F'[: ]+' '/Link encap/{a=$1;next;}/inet addr/{if($4!~/^127\./ && $4!~/^192\./ && $4 !~/^10\./ &&$4!~/^172\./)print $4}'|head -1`
-#
+#检测是否安装ntpdate
 ls /sbin/ntpdate
 if [ $? -ne 0 ];then
 	yum -y install ntpdate
 fi
-echo 'ZONE="Asia/Shanghai"' >/etc/sysconfig/clock &&ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && /usr/sbin/ntpdate time.nist.gov
-
+#修改时区
+echo 'ZONE="Asia/Shanghai"' >/etc/sysconfig/clock && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && /usr/sbin/ntpdate time.nist.gov#
+#time.nist.gov
 /usr/sbin/ntpdate time.nist.gov
 if [ $? -ne 0 ];then
     /usr/sbin/ntpdate time-a.nist.gov
